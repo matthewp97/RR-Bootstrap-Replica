a6df65b91a273124795ee6f8045446692f26e946
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-10-21 16:03:50 -0700

    nav bar set up with buttons right aligned

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import TopBar from './components/TopBar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
     </div>
   );
 }
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,28 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+
+function TopBar(){
+    return(
+        <Navbar bg="light" expand="lg" >
+        <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+            <img src="[Instacart logo src]" alt="instacart logo" /> 
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+                <Nav.Link href="#login">Log in</Nav.Link>
+                <Button variant="success"> Sign up </Button>{' '}
+            </Nav>
+            
+        
+        </Navbar.Collapse>
+    </Navbar>
+    
+    )
+};
+export default TopBar
